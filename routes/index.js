dd7aa61ef3b243f931be7bfad914d7d9ea712810
PeterJForsberg <PeterJohnForsberg@gmail.com>
2016-08-05 09:57:51 -0500

    junk drawer and progress included

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-var twitterdb = require('../db');
+var client = require('../db');
 
 module.exports = function makeRouterWithSockets (io) {
 
   // a reusable function
   function respondWithAllTweets (req, res, next){
-    var allTheTweets = tweetBank.list();
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: allTheTweets,
-      showForm: true
+  //   var allTheTweets = tweetBank.list();
+  //   res.render('index', {
+  //     title: 'Twitter.js',
+  //     tweets: allTheTweets,
+  //     showForm: true
+  //   });
+      client.query('SELECT * FROM tweets INNER JOIN users ON tweets.id = users.id ', function (err, result) {
+      if (err) return next(err); // pass errors to Express
+      var tweets = result.rows;
+      res.render('index', { 
+        title: 'Twitter.js', 
+        tweets: tweets, 
+        showForm: true });
     });
   }
 
@@ -21,14 +29,28 @@ module.exports = function makeRouterWithSockets (io) {
 
   // single-user page
   router.get('/users/:username', function(req, res, next){
-    var tweetsForName = tweetBank.find({ name: req.params.username });
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsForName,
-      showForm: true,
-      username: req.params.username
+    var name = req.params.username;
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.id = users.id WHERE name=$1', [name], function (err, result) {
+      if (err) return next(err);
+      var tweets = result.rows;
+        console.log(tweets);
+
+      res.render('index', {
+        title: 'Twitter.js', 
+        tweets: tweets, 
+        showForm: true 
+      })
     });
   });
+
+    // var tweetsForName = tweetBank.find({ name: req.params.username });
+    // res.render('index', {
+    //   title: 'Twitter.js',
+    //   tweets: tweetsForName,
+    //   showForm: true,
+    //   username: req.params.username
+    // });
+  // });
 
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
@@ -46,16 +68,7 @@ module.exports = function makeRouterWithSockets (io) {
     res.redirect('/');
   });
 
-  // // replaced this hard-coded route with general static routing in app.js
-  // router.get('/stylesheets/style.css', function(req, res, next){
-  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
-  // });
 
-  client.query('SELECT * FROM tweets', function (err, result) {
-    if (err) return next(err); // pass errors to Express
-    var tweets = result.rows;
-    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
-  });
 
   return router;
 }
